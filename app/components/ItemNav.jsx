049029c8466a55dfefec9dc395351345098b3449
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-04-08 02:18:07 -0700

    Altered nav component

--- app/components/ItemNav.jsx
+++ app/components/ItemNav.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ItemNav({ number, pageTitle, path }) {
   return (
-    <li className=" text-cyan-50">
+    <li className=" text-cyan-50 hover:border-b-2">
       <Link href={path} className="flex gap-3">
         <span>{number}</span> <span>{pageTitle}</span>{" "}
       </Link>
