b0589174e3a69e954e07e712e6b0a29faef4b6fa
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-10-25 05:24:23 -0700

    Updated component for header

--- app/components/ItemNav.jsx
+++ app/components/ItemNav.jsx
@@ -1,11 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function ItemNav({ number, pageTitle, path }) {
+  const pathname = usePathname();
+
   return (
-    <li className=" text-cyan-50 hover:border-b-2">
+    <li
+      className={`link ${
+        pathname === path
+          ? "active border-b-4 text-white border-white tracking-widest flex items-center h-full hover:border-b-4 gap-3"
+          : "text-white hover:border-b-4 hover:border-gray-500 tracking-widest flex items-center h-full"
+      }`}
+    >
       <Link href={path} className="flex gap-3">
-        <span>{number}</span> <span>{pageTitle}</span>{" "}
+        <span className="font-bold ">{number}</span>{" "}
+        <span className=" ">{pageTitle}</span>{" "}
       </Link>
     </li>
   );
 }
+
+// text-white hover:border-b-2 tracking-widest flex items-center h-full
+
+// className={
+//   router.pathname == path
+//     ? "active text-white hover:border-b-2 tracking-widest flex items-center h-full"
+//     : "text-white hover:border-b-2 border-gray-200 tracking-widest flex items-center h-full"
+// }
+
+// className={`link ${pathname === path ? "active text-black" : ""}`}
+
+// flex gap-3
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
   const logoImage = "../../public/images/assets/shared/logo.svg";
   return (
     <header className="flex mt-10 ml-10 mr-0">
-      <div className=" flex-1">
+      <div className=" flex flex-row flex-1">
         <Link href="/">
           <Image
             src="/images/assets/shared/logo.svg"
@@ -16,12 +16,15 @@ export default function NavBar() {
             className=""
           />
         </Link>
+        <div className="flex items-center pl-10">
+          <div className=" h-px w-5/11 bg-gray-500 absolute ml-5 z-10"></div>
+        </div>
       </div>
       <nav
         className="flex flex-1 items-center justify-start pl-5"
         style={{ backdropFilter: "blur(100px)" }}
       >
-        <ul className=" flex items-center ml-5 gap-8">
+        <ul className=" flex items-center ml-24 gap-8 h-full">
           <ItemNav number="00" pageTitle="HOME" path="/" />
           <ItemNav number="01" pageTitle="DESTINATION" path="/destination" />
           <ItemNav number="02" pageTitle="CREW" path="/crew" />
